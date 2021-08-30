de53324583f8b29b411a6a58ef77e13a2c861edf
Elena Shustova <alenyf-ka@mail.ru>
2021-08-30 23:32:14 +0300

    complete part of task

--- src/index.js
+++ src/index.js
@@ -4,7 +4,53 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    const operators = {
+        '+': (x, y) => x + y,
+        '-': (x, y) => x - y,
+        '*': (x, y) => x * y,
+        '/': (x, y) => x / y,
+    };
+    
+        let result =[];
+        let stack = [];
+        
+        let newExpr = expr.split(' ');
+      
+        
+        for (let i = 0; i < newExpr.length; i++) {
+            if (newExpr[i] === '') {
+                delete newExpr[i];
+                continue;
+            }
+
+            if (newExpr[i] === '(') {
+                stack.push();
+            } else if (Object.keys(operators).includes(newExpr[i])) {
+                stack.push(newExpr[i]);
+            } else {
+                result.push(parseInt(newExpr[i]));
+            }
+
+            if (newExpr[i] === ')') {
+                if (result.length > 1) {
+                let [y, x] = [result.pop(), result.pop()];
+                result.push(operators[newExpr[i]](x, y));
+                stack.pop();
+                } 
+            }
+
+            if (result.length === 1) {
+                continue;
+            }
+            if (result.length > 1) {
+                let [y, x] = [result.pop(), result.pop()];
+                result.push(operators[stack[stack.length - 1]](x, y));
+            }
+        }
+        
+        result = result.pop();
+        return result;
+   
 }
 
 module.exports = {
